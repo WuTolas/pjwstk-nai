3512a96b79b8b842db2a472fa88f48d4d7794025
WuTolas <36102576+WuTolas@users.noreply.github.com>
2020-11-27 01:26:58 +0100

    #3 babushka init

---
+++ lab-3/__init__.py
@@ -0,0 +1,3 @@
+# Problem: Babushka fuzzy logic - how much money I should give to my grandson
+# Authors: Damian Rutkowski (s16583), Piotr Krajewski (s17410)
+# Environment setup: https://github.com/WuTolas/pjwstk-nai/tree/main/lab-3/README.md
