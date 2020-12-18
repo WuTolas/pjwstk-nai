20d759004a7daeb7187f550e619fb4d810da75d1
WuTolas <wut1337@hotmail.com>
2020-12-18 21:56:30 +0100

    #4 print changes

--- lab-4/main.py
+++ lab-4/main.py
@@ -23,24 +23,23 @@ def main():
     full_name = str(input("Type full name of the person you want to see movie recommendations: "))
     recommendations = movie_service.recommend_movies(full_name)
 
+    print_headline(full_name)
+
     if len(recommendations.must_watch_movies) == 0 and len(recommendations.avoid_movies) == 0:
-        print("Looks like there is not enough data about this person to show you recommendations :(") 
+        print_headline("Looks like there is not enough data about this person to generate recommendations :(") 
     else:
-        print("**************************************************************************************************************************************")
-        print("*************************************************** YOU SHOULD CHECK THESE MOVIES: ***************************************************")
-        print("**************************************************************************************************************************************")
+        print_headline("YOU SHOULD CHECK THESE MOVIES:")
         for m in recommendations.must_watch_movies:
             print("-------------------------------------------------")
             print_movie(m)
             print("-------------------------------------------------")
 
-        print("**************************************************************************************************************************************")
-        print("*************************************************** AND BETTER AVOID THESE MOVIES: ***************************************************")
-        print("**************************************************************************************************************************************")
+        print_headline("AND BETTER AVOID THESE MOVIES:")
         for m in recommendations.avoid_movies:
             print("-------------------------------------------------")
             print_movie(m)
             print("-------------------------------------------------")
+
 
 def print_movie(movie_info):
     """
@@ -60,6 +59,19 @@ def print_movie(movie_info):
     print("Description: " + str(movie_info.description))
     print("Genre: " + str(movie_info.genre))
     print("Production: " + str(movie_info.production_country))
+
+
+def print_headline(message):
+    """
+    Prints the headline.
+
+    Parameters
+    ----------
+    message : str
+    """
+    print("**************************************************************************************************************************************")
+    print("*************************************************** " + message + " ***************************************************")
+    print("**************************************************************************************************************************************")
 
 
 if __name__ == "__main__":
